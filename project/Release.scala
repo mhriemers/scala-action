cbe07010d8f9008eba0985e216c2a193c7f5a4d1
Martijn Riemers <martijn@riemers.com>
2022-08-03 01:11:05 +0200

    Removed target

--- project/Release.scala
+++ project/Release.scala
@@ -1,16 +1,32 @@
-import sbt.Project
+import sbt.{Project, State}
 import sbtrelease.ReleasePlugin.autoImport.ReleaseKeys.versions
 import sbtrelease.ReleasePlugin.autoImport.{ReleaseStep, releaseVcs, releaseVcsSign}
-import sbtrelease.Version
+import sbtrelease.{Vcs, Version}
 
 object Release {
 
-  val addTarget: ReleaseStep = ReleaseStep(st => {
-    val vcs = Project.extract(st)
-      .get(releaseVcs)
-      .getOrElse(sys.error("Aborting release. Working directory is not a repository of a recognized VCS."))
+  private def vcs(state: State): Vcs = Project.extract(state)
+    .get(releaseVcs)
+    .getOrElse(sys.error("Aborting release. Working directory is not a repository of a recognized VCS."))
 
-    vcs.cmd("add", "--force", "target").!
+  val addTarget: ReleaseStep = ReleaseStep(st => {
+    vcs(st).cmd("add", "--force", "target").!
+    st
+  })
+
+  val commitBuild: ReleaseStep = ReleaseStep(st => {
+    val build = vcs(st).cmd("rev-parse", "--short", "HEAD").!!
+    vcs(st).cmd("commit", "--allow-empty", "-m", s"Build for $build")
+    st
+  })
+
+  val createTempBranch: ReleaseStep = ReleaseStep(st => {
+    vcs(st).cmd("checkout", "-b", "temp").!
+    st
+  })
+
+  val deleteTempBranch: ReleaseStep = ReleaseStep(st => {
+    vcs(st).cmd("branch", "-d", "temp").!
     st
   })
 
@@ -18,13 +34,10 @@ object Release {
     val raw = st.get(versions)
       .getOrElse(sys.error("No versions are set! Was this release part executed before inquireVersions?"))._1
     val version = Version(raw).map(_.major).getOrElse(sys.error("Version couldn't be parsed!"))
-    val ex = Project.extract(st)
-    val vcs = ex
-      .get(releaseVcs)
-      .getOrElse(sys.error("Aborting release. Working directory is not a repository of a recognized VCS."))
-    val sign = ex.get(releaseVcsSign)
+    val sign = Project.extract(st).get(releaseVcsSign)
 
-    vcs.tag(s"v$version", s"Releasing $version", sign).!
+    vcs(st).cmd("tag", "-f", "-a", "-m", s"Update v$version", s"v$version")
+    vcs(st).cmd("push", "origin", s"v$version", "--force")
     st
   })
 
